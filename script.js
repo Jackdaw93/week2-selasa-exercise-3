9b109b430595e5da894f0b52a0b1483f4f630f6f
Rohmad <rohmad37@ymail.com>
2020-06-16 15:24:40 +0700

    Exercise Selasa 3

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Cara 1*/
+
+const element = document.querySelector("div h1");
+
+element.style.color = "blue";
+element.style.backgroundColor = "yellow";
+element.style.fontSize = "90px";
+element.style.fontSize = "Arial";
+element.style.textAlign = "center";
+
+/*Cara 2 */
+/*
+document.getElementById("element").style.color = "blue";
+document.getElementById("element").style.fontFamily = "Arial";
+document.getElementById("element").style.fontSize = "90px";
+document.getElementById("element").style.textAlign = "center";
+document.getElementById("element").style.backgroundColor = "yellow";
+*/
